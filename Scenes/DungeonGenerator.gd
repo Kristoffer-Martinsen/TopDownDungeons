47fa3f9c88795abdd37e929b493bf635d5814dd8
Kristoffer Martinsen <kristoffermartinsen.95@gmail.com>
2024-03-21 22:39:53 +0100

    Room creation and WIP corridor pathfinding

--- Scenes/DungeonGenerator.gd
+++ Scenes/DungeonGenerator.gd
@@ -1,58 +1,55 @@
 extends Node2D
 
 @onready var tile_map: TileMap = $"TileMap"
-
-#Get a circle
-#Get random point in circle
-#Create rooms with a random width and a random height
-#  -> get point in tilemap
-#  -> loop over rectangle based on height and width
-#  -> change tile to floor
-var rng = RandomNumberGenerator.new()
-var cells: Array[Vector2i]
+@export var width: int = 100
+@export var height: int = 100
+var rng: RandomNumberGenerator = RandomNumberGenerator.new()
+var wall_cells: Array[Vector2i]
+var floor_cells: Array[Vector2i]
+var room_center_array: Array[Vector2i]
 
 
 func _ready() -> void:
+	Pathfinding.set_tile_map(tile_map)
 	randomize()
-	var rr = 100
-	var random_point = get_random_point_in_circle(rr)
-	place_walls(rr)
+	place_walls()
+	for i in 50:
+		create_room(choose_start_point())
+	_connect_room_centers()
+	tile_map.set_cells_terrain_connect(0, wall_cells, 0, 0)
+	tile_map.set_cells_terrain_connect(0, floor_cells, 0, 1)
 
-	create_room(random_point)
+func choose_start_point() -> Vector2i:
+	return Vector2i(rng.randi_range(1, width), rng.randi_range(1, height))
 
-	# random_point = get_random_point_in_circle(rr)
-	tile_map.set_cells_terrain_connect(0, cells, 0, 0)
+func create_room(tile_position: Vector2i) -> void:
+	var min_room_width: int = 4
+	var max_room_width: int = 10
+	var min_room_height: int = 4
+	var max_room_height: int = 10
 
-func get_random_point_in_circle(radius: int) -> Vector2:
-	var theta = 2*PI*rng.randf()
-	var u = rng.randf()+rng.randf()
-	var r = null
+	var room_width: int = rng.randi_range(min_room_width, max_room_width)
+	var room_height: int = rng.randi_range(min_room_height, max_room_height)
 
-	if u > 1:
-		r = 2-u
-	else:
-		r = u
-	
-	return Vector2(radius*r*cos(theta), radius*r*sin(theta))
+	for w in room_width:
+		for h in room_height:
+			floor_cells.append(Vector2i(w + tile_position.x, h + tile_position.y))
+			tile_map.set_cell(0, Vector2i(w + tile_position.x, h + tile_position.y), 1, Vector2i(1,0))
+			wall_cells.erase(Vector2i(w + tile_position.x, h + tile_position.y))
 
-func create_room(position: Vector2) -> void:
-	var tile_map_coords: Vector2i = tile_map.local_to_map(position.round())
-	var min_width: int = 4
-	var max_width: int = 10
-	var min_height: int = 4
-	var max_height: int = 10
-
-	var width: int = rng.randi_range(min_width, max_width)
-	var height: int = rng.randi_range(min_height, max_height)
-
-	for w in width:
-		for h in height:
-			tile_map.set_cell(0, Vector2i(w + tile_map_coords.x, h + tile_map_coords.y), 1, Vector2i(1,0))
-			# cells.erase(Vector2i(w + tile_map_coords.x, h + tile_map_coords.y))
+	var center_of_room: Vector2i = Vector2i(floor(tile_position.x + room_width / 2), floor(tile_position.y + room_height / 2))
 	pass
 
-func place_walls(radius: int) -> void:
-	for x in range(-radius, radius):
-		for y in range(-radius, radius):
-			cells.append(Vector2i(x,y))
+func _connect_room_centers() -> void:
+	while room_center_array.size() > 0:
+		var start_center: Vector2i = room_center_array.pop_front()
+		var next_center: Vector2i = room_center_array[0]
+		
+	pass
+
+func place_walls() -> void:
+	for x in range(height):
+		for y in range(width):
+			wall_cells.append(Vector2i(x,y))
 			tile_map.set_cell(0, Vector2i(x, y), 0, Vector2i(0,0))
+
