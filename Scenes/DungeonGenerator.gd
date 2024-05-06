314b9d34a2aa7d3c07f1278b91939c871141125f
Kristoffer Martinsen <kristoffermartinsen.95@gmail.com>
2024-05-06 18:18:03 +0200

    CorridorGen WIP

--- Scenes/DungeonGenerator.gd
+++ Scenes/DungeonGenerator.gd
@@ -23,6 +23,8 @@ func _ready() -> void:
 	place_walls()
 	for i in number_of_rooms:
 		create_room(choose_start_point())
+	print(room_center_array)
+	print("--------------------------------------------------")
 	_connect_room_centers()
 	tile_map.set_cells_terrain_connect(0, wall_cells, 0, 0)
 	tile_map.set_cells_terrain_connect(0, floor_cells, 0, 1)
@@ -39,20 +41,45 @@ func create_room(tile_position: Vector2i) -> void:
 
 	var room_width: int = rng.randi_range(min_room_width, max_room_width)
 	var room_height: int = rng.randi_range(min_room_height, max_room_height)
-	for w in room_width:
-		for h in room_height:
-			if first_room:
-				spawn_location = Vector2(w + tile_position.x * 16 + 8, h + tile_position.y * 16 + 8)
-				first_room = false
-				emit_signal("character_spawn", spawn_location)
-			floor_cells.append(Vector2i(w + tile_position.x, h + tile_position.y))
-			tile_map.set_cell(0, Vector2i(w + tile_position.x, h + tile_position.y), 1, Vector2i(1,0))
-			wall_cells.erase(Vector2i(w + tile_position.x, h + tile_position.y))
-	var center_of_room: Vector2i = Vector2i(floor(tile_position.x + room_width / 2), floor(tile_position.y + room_height / 2))
-	room_center_array.append(center_of_room)
+	
+	if _validate_room_placement(tile_position, room_width, room_height):
+		for w in room_width:
+			for h in room_height:
+				if first_room:
+					spawn_location = Vector2(w + tile_position.x * 16 + 8, h + tile_position.y * 16 + 8)
+					first_room = false
+					emit_signal("character_spawn", spawn_location)
+				floor_cells.append(Vector2i(w + tile_position.x, h + tile_position.y))
+				tile_map.set_cell(0, Vector2i(w + tile_position.x, h + tile_position.y), 1, Vector2i(1,0))
+				wall_cells.erase(Vector2i(w + tile_position.x, h + tile_position.y))
+		var center_of_room: Vector2i = Vector2i(floor(tile_position.x + room_width / 2), floor(tile_position.y + room_height / 2))
+		room_center_array.append(center_of_room)
+
 
 func _connect_room_centers() -> void:
+	var first_room: Vector2i
+	var second_room: Vector2i
+	for room in room_center_array:
+		if room_center_array.size() > 1:
+			first_room = room_center_array.pop_front()
+			second_room = room_center_array.front()
+		var corridor_path: Array[Vector2i] = pathfinding.get_move_path(first_room, second_room)
+		for c in corridor_path:
+			tile_map.set_cell(0, c, 1, Vector2i(1,0))
+		print(corridor_path)
+		print("------------------------------------------------------------")
 	pass
+
+func _validate_room_placement(starting_tile: Vector2i, room_w: int, room_h: int) -> bool:
+	if starting_tile.x + room_w >= Globals.ASTAR_DIMENSIONS.size.x or starting_tile.y + room_h >= Globals.ASTAR_DIMENSIONS.size.y:
+		return false
+	
+	if tile_map.get_cell_tile_data(0, Vector2i(starting_tile.x, starting_tile.y)).get_custom_data("wall") == true \
+	and tile_map.get_cell_tile_data(0, Vector2i(starting_tile.x + room_w, starting_tile.y)).get_custom_data("wall") == true \
+	and tile_map.get_cell_tile_data(0, Vector2i(starting_tile.x, starting_tile.y + room_h)).get_custom_data("wall") == true \
+	and tile_map.get_cell_tile_data(0, Vector2i(starting_tile.x + room_w, starting_tile.y + room_h)).get_custom_data("wall") == true:
+		return true
+	return false
 
 func place_walls() -> void:
 	for x in range(Globals.ASTAR_DIMENSIONS.size.x):
