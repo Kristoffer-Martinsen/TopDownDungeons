53dd178c92a533f28f6bf498b711d115ccc858c1
Kristoffer Martinsen <kristoffermartinsen.95@gmail.com>
2024-05-17 14:50:56 +0200

    pathfinding map coords
    Pathfinding now expects start and end coords to be based on tilemap coords

--- Utility/Pathfinding/PathFinding.gd
+++ Utility/Pathfinding/PathFinding.gd
@@ -8,12 +8,11 @@ var next_point_in_path: Vector2
 func get_move_path(current_position: Vector2, end_position: Vector2) -> Array[Vector2i]:
 	var id_path: Array[Vector2i]
 	id_path = Globals.ASTAR_GRID.get_id_path(
-		DungeonGenerator.tile_map.local_to_map(current_position),
-		DungeonGenerator.tile_map.local_to_map(end_position))
+		current_position, end_position
+	)
 	if id_path.is_empty() == false:
 		current_id_path = id_path
 		current_point_path = Globals.ASTAR_GRID.get_point_path(
-			DungeonGenerator.tile_map.local_to_map(current_position),
-			DungeonGenerator.tile_map.local_to_map(end_position)
+			current_position, end_position
 		)
 	return id_path
